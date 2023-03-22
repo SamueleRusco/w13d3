06a0694d842fa8461013e6dc725786de643bc76f
SamueleRusco <119084990+SamueleRusco@users.noreply.github.com>
2023-03-22 17:07:18 +0100

    ex2ok

--- src/es2/MainEs2.java
+++ src/es2/MainEs2.java
@@ -1,4 +1,34 @@
 package es2;
 
+import es1.ThreadEs1;
+
 public class MainEs2 {
+    public static long sommaNumeriTotale;
+
+    public static void main(String[] args) {
+        Thread t1 = new arrayOfRandomNumberGenerator("1");
+        Thread t2 = new arrayOfRandomNumberGenerator("2");
+        Thread t3 = new arrayOfRandomNumberGenerator("3");
+        Thread t4 = new arrayOfRandomNumberGenerator("4");
+        Thread t5 = new arrayOfRandomNumberGenerator("5");
+
+        try{
+        t1.start();
+        t1.join();
+        t2.start();
+        t2.join();
+            t3.start();
+            t3.join();
+            t4.start();
+            t4.join();
+            t5.start();
+            t5.join();
+
+            System.out.println("la somma dei numeri è: " + sommaNumeriTotale);
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
 }
---
+++ src/es2/arrayOfRandomNumberGenerator.java
@@ -0,0 +1,28 @@
+package es2;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+public class arrayOfRandomNumberGenerator extends Thread{
+        String stringa;
+        static Random r = new Random();
+        static int number;
+        static List<Integer> arrayNumeri = new ArrayList<>();
+        public arrayOfRandomNumberGenerator(String s){
+            this.stringa = s;
+        }
+        @Override
+        public void run() {
+            int sommaNumeri = 0;
+            for (int i = 0; i < 100; i++) {
+                int number = r.nextInt(100);
+                sommaNumeri = sommaNumeri + number;
+            }
+            MainEs2.sommaNumeriTotale = MainEs2.sommaNumeriTotale + sommaNumeri;
+            arrayNumeri.add(sommaNumeri);
+
+
+            System.out.println("fine del thread " + stringa +", la somma è: " + sommaNumeri + arrayNumeri);
+
+        }
+    }
+
